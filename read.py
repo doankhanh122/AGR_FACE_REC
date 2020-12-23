79eabdce245b5b3f70d907505e26acb871bec5fb
doankhanh122 <doankhanh122@gmail.com>
2020-12-23 19:38:52 +0700

    Read photo and video commit

--- read.py
+++ read.py
@@ -1,2 +1,24 @@
 import cv2 as cv
-# Let start
+
+
+# Reading image
+################
+# img = cv.imread('Photos/cat2.jpg')
+# cv.imshow("Cat", img)
+
+
+# Reading video
+################
+# capture = cv.VideoCapture(0) for webcam
+capture = cv.VideoCapture("Videos/video2.mp4")
+
+while True:
+    isTrue, frame = capture.read()
+    cv.imshow("Video", frame)
+
+    if cv.waitKey(20) & 0xFF == ord('d'):
+        break
+
+capture.release()
+cv.destroyAllWindows()
+cv.waitKey(0)
